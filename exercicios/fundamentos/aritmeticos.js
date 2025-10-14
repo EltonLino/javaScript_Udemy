1cb7942223cf1e0f966848820fd6da5fed374e2d
Elton Lino <eltonblino@gmail.com>
2025-10-14 06:58:04 -0300

    operadores

---
+++ exercicios/fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15] // atribuindo com destructuring
+
+const soma = a + b + c + d //adição
+const subtracao = d - b //  subtração
+const multiplicacao = a * b // multiplicação
+const divisao = d / a // divisão
+const modulo = a % 2 // modulo - resto da divisão
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
---
+++ exercicios/fundamentos/ternario.js
@@ -0,0 +1,5 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' // Operador ternário
+// função arrow   var arrow lógica op. verdadeiro  falsa
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
